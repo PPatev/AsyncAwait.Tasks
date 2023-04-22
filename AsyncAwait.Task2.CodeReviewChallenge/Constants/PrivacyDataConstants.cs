86a439ba03b064e6e1813865e7f09aaee6ae11d6
Peter Patev <petar1patev@gmail.com>
2023-04-22 22:34:44 +0300

    Task 2 refactor and theary

---
+++ AsyncAwait.Task2.CodeReviewChallenge/Constants/PrivacyDataConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace AsyncAwait.Task2.CodeReviewChallenge.Constants
+{
+    public class PrivacyDataConstants
+    {
+        public static readonly string PersonalDataPolicy = "This Policy describes how async/await processes your personal data, but it may not address all possible data processing scenarios.";
+    }
+}
